bdb0bb3b7be1912d8517889ae04fa1c4fc1a244e
Danielle <37205880+DanniDoggo@users.noreply.github.com>
2020-01-25 23:51:15 -0500

    Update App.js

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { HomePage } from './HomePage.js';
+import { HomePage } from './components/HomePage/HomePage.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
         </p>
         <a
