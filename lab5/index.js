97ac8602ce984be3dc3e5db0fcad3ce7609c4c88
Danielle <37205880+DanniDoggo@users.noreply.github.com>
2020-02-15 21:04:18 -0500

    added lab5

---
+++ lab5/index.js
@@ -0,0 +1,63 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const pokemons = require('json-pokemon');
+
+const chalk = require("chalk");
+
+//const fs = require("fs");
+//let fileContents = fs.readFileSync("database.json");
+//let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+
+App.get("/id/:id", (req, res) => {
+
+let result = {"error": "Error! It's not very effective!"};
+
+  pokemons.forEach((value)=>{
+    if(value.id == req.params.id){
+      result = value;
+    }
+  });
+
+if(result.error){
+console.log(chalk.red(req.path));
+
+}
+ else {
+  console.log(chalk.green(req.path));
+ }
+
+  //res.send(result);
+  res.json(result);
+
+});
+
+App.get("/name/:name", (req, res) => {
+
+let result = {"error": "Error! It's not very effective!"};
+
+  pokemons.forEach((value)=>{
+    if(value.name == req.params.name){
+      result = value;
+    }
+  });
+
+if(result.error){
+console.log(chalk.red(req.path));
+
+}
+ else {
+  console.log(chalk.green(req.path));
+ }
+
+  //res.send(result);
+  res.json(result);
+
+});
+
+App.listen(port,() => {
+  console.log("Server used running! It's super effective!");
+});
