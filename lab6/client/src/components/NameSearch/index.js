fb76d2bd08ac0d15de9aef55b7abd1a6a1292b19
Danielle <37205880+DanniDoggo@users.noreply.github.com>
2020-02-22 23:28:01 -0500

    uploaded lab6

---
+++ lab6/client/src/components/NameSearch/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class NameSearch extends React.Component {
+
+    readName(event) {
+
+        event.preventDefault();
+
+        let element = document.querySelector("#NAME");
+
+        fetch("/name/" + element.value)
+
+                .then((res) => {
+                      return res.json();
+                })
+
+                .then((processed) => {
+                      let reporting = document.querySelector("#reportingArea");
+
+                      if(processed.error) {
+                          reporting.innerHTML = processed.error;
+                        }
+
+                      else {
+                              reporting.innerHTML = processed.name;
+                        }
+                    });
+                  element.value = "";
+                  }
+
+    render() {
+        return(
+            <div>
+                 <h2>Pokemon Name</h2>
+                 <form onSubmit={this.readName}>
+                    <input id="NAME" type="text" />
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default NameSearch;
---
+++ lab6/index.js
@@ -0,0 +1,62 @@
+const cors = require("cors");
+App.use(cors());
+
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const pokemons = require('json-pokemon');
+
+const chalk = require("chalk");
+
+App.use("/", Express.static("client/build"));
+
+App.get("/id/:id", (req, res) => {
+
+let result = {"error": "Error! It's not very effective!"};
+
+  pokemons.forEach((value)=>{
+    if(value.id == req.params.id){
+      result = value;
+    }
+  });
+
+if(result.error){
+console.log(chalk.red(req.path));
+
+}
+ else {
+  console.log(chalk.green(req.path));
+ }
+
+  //res.send(result);
+  res.json(result);
+
+});
+
+App.get("/name/:name", (req, res) => {
+
+let result = {"error": "Error! It's not very effective!"};
+
+  pokemons.forEach((value)=>{
+    if(value.name == req.params.name){
+      result = value;
+    }
+  });
+
+if(result.error){
+console.log(chalk.red(req.path));
+
+}
+ else {
+  console.log(chalk.green(req.path));
+ }
+
+  //res.send(result);
+  res.json(result);
+
+});
+
+App.listen(port,() => {
+  console.log("Server used running! It's super effective!");
+});
